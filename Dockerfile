66f0bee171f40023e1c8f0a8029870927487fc0c
Andy Thorne <contrabandvr@gmail.com>
2024-08-07 19:19:29 +0100

    Added github app registration

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
-FROM ubuntu
+FROM ubuntu:22.04
+
+ARG DEBIAN_FRONTEND=noninteractive
 
 RUN useradd -m actions
-RUN apt-get -y update && apt-get install -y \
-    apt-transport-https ca-certificates curl jq software-properties-common \
-    && toolset="$(curl -sL https://raw.githubusercontent.com/actions/runner-images/main/images/ubuntu/toolsets/toolset-2004.json)" \
+RUN apt-get -y update && apt-get install -y apt-transport-https ca-certificates curl wget jq software-properties-common \
+    && toolset="$(curl -sL https://raw.githubusercontent.com/actions/runner-images/main/images/ubuntu/toolsets/toolset-2204.json)" \
     && common_packages=$(echo $toolset | jq -r ".apt.common_packages[]") && cmd_packages=$(echo $toolset | jq -r ".apt.cmd_packages[]") \
     && for package in $common_packages $cmd_packages; do apt-get install -y --no-install-recommends $package; done
 
@@ -22,10 +23,19 @@ RUN add-apt-repository ppa:git-core/ppa -y \
 # Install LTS Node.js and related build tools
 RUN curl -sL https://raw.githubusercontent.com/mklement0/n-install/stable/bin/n-install | bash -s -- -ny - \
     && ~/n/bin/n lts \
-    && npm install -g grunt gulp n parcel-bundler typescript newman \
-    && npm install -g --save-dev webpack webpack-cli \
-    && npm install -g npm \
+    && npm install -g npm pnpm nx@latest \
     && rm -rf ~/n
+
+# Install Docker
+RUN install -m 0755 -d /etc/apt/keyrings \
+    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc \
+    && chmod a+r /etc/apt/keyrings/docker.asc \
+    && echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
+    && apt-get update \
+    && apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 WORKDIR /home/actions/actions-runner
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-if [ -z "${TOKEN:-}" ]
+if [ -z "${APP_INSTALLATION_ID:-}" ]
 then
-  echo "TOKEN is required"
+  echo "APP_INSTALLATION_ID is required"
+  exit 1
+fi
+
+if [ -z "${APP_CLIENT_ID:-}" ]
+then
+  echo "APP_CLIENT_ID is required"
   exit 1
 fi
 
@@ -20,7 +26,60 @@ else
   exit 1
 fi
 
+# See https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-a-json-web-token-jwt-for-a-github-app
+
+pem=$( echo $APP_PRIVATE_KEY | tr -d '\r' | base64 -d )
+
+now=$(date +%s)
+iat=$((${now} - 60)) # Issues 60 seconds in the past
+exp=$((${now} + 600)) # Expires 10 minutes in the future
+
+b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
+
+header_json='{
+    "typ":"JWT",
+    "alg":"RS256"
+}'
+# Header encode
+header=$( echo -n "${header_json}" | b64enc )
+
+payload_json='{
+    "iat":'${iat}',
+    "exp":'${exp}',
+    "iss":"'${APP_CLIENT_ID}'"
+}'
+# Payload encode
+payload=$( echo -n "${payload_json}" | b64enc )
+
+# Signature
+header_payload="${header}"."${payload}"
+signature=$(
+    openssl dgst -sha256 -sign <(echo -n "${pem}") \
+    <(echo -n "${header_payload}") | b64enc
+)
+
+# Create JWT
+APP_JWT="${header_payload}"."${signature}"
+
+TOKEN=$(curl --request POST \
+--url "https://api.github.com/app/installations/${APP_INSTALLATION_ID}/access_tokens" \
+--header "Accept: application/vnd.github+json" \
+--header "Authorization: Bearer ${APP_JWT}" \
+--header "X-GitHub-Api-Version: 2022-11-28" | jq -r .token)
+
+if [[ -z "${TOKEN}" ]] || [[ "${TOKEN}" = "null" ]]
+then
+  echo "Failed to get access token"
+  exit 1
+fi
+
 RUNNER_TOKEN=$(curl -s -X POST -H "authorization: token ${TOKEN}" "https://api.github.com/${API_PATH}/actions/runners/registration-token" | jq -r .token)
+
+if [ -z "${RUNNER_TOKEN}" ]
+then
+  echo "Failed to get runner token"
+  exit 1
+fi
 
 cleanup() {
   ./config.sh remove --token "${RUNNER_TOKEN}"
@@ -30,6 +89,7 @@ cleanup() {
   --url "https://github.com/${CONFIG_PATH}" \
   --token "${RUNNER_TOKEN}" \
   --name "${NAME:-$(hostname)}" \
+  --labels "self-hosted,ubuntu" \
   --unattended
 
 trap 'cleanup' SIGTERM
